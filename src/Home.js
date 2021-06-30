0e8c0bb91db212b97f1bc10aacb2a1309db94b0f
AsaBrown <43590260+AsaBrown@users.noreply.github.com>
2021-06-30 19:41:05 -0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Login } from './components/Login'
+// import { Login } from './components/Login'
 import QueryForm from './components/QueryForm'
 import { ViewLogs } from './components/ViewLogs';
 import { Info } from './components/Info'
