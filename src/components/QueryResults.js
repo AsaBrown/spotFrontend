ce7b242138a6ceca8bbbae3a889db0019c2b4536
AsaBrown <43590260+AsaBrown@users.noreply.github.com>
2021-06-30 19:45:04 -0400

    Update QueryResults.js

--- src/components/QueryResults.js
+++ src/components/QueryResults.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Griddle, {RowDefinition, ColumnDefinition} from 'griddle-react';
 import { selectors } from 'griddle-react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import '../css/queryresults.css';
 
 export const QueryResults = (props) => {
