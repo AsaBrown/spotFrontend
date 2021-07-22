dd238ed1ac58f45b8f2c4ed00f69ad513a433bbe
AsaBrown <43590260+AsaBrown@users.noreply.github.com>
2021-07-21 20:09:43 -0400

    added login implementation

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,15 +10,58 @@ function doLogin(e) {
         });
   }
 
+const doSiteLogin = (e) => {
+    e.preventDefault();
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/authentication/login`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: document.getElementById('loginUsername').value,
+            password: document.getElementById('loginPassword').value
+        })
+    }).then(response => response.text()).then(body => {
+        console.log(body);
+        // window.location.replace(body);
+        // store jwt token in cookies
+    });
+}
+const doSiteSignup = (e) => {
+    e.preventDefault();
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/authentication/signUp`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: document.getElementById('signupUsername').value,
+            password: document.getElementById('signupPassword').value
+        })
+    }).then(response => response.text()).then(body => {
+        //display account created successfully, redirect to login
+    });
+}
+
 export const Login = () => {
     return (
         <div id='login'>
             <form>
-                <input type="text" id="user" name="user" placeholder="Username" />
+                <input type="text" id="loginUsername" name="loginUsername" placeholder="Username" />
                 <br/>
-                <input type="text" id="pass" name="pass" placeholder="Password" />
+                <input type="text" id="loginPassword" name="loginPassword" placeholder="Password" />
                 <br />
-                <button type="submit">Login</button>
+                <button className='myButton' onClick={doSiteLogin}>Login to site</button>
+            </form>
+            <br /><br />
+            <form>
+                <input type="text" id="signupUsername" name="user" placeholder="Username" />
+                <br/>
+                <input type="text" id="signupPassword" name="pass" placeholder="Password" />
+                <br />
+                <button className='myButton' onClick={doSiteSignup}>Signup for site</button>
             </form>
             <br /><br />
             <form>
@@ -26,7 +69,6 @@ export const Login = () => {
                 <br/>
                 <input type="text" id="pass" name="pass" placeholder="Password" />
                 <br />
-                <button type="submit">Sign Up</button>
                 <button className='myButton' onClick={doLogin}>Query Songs</button>
             </form>
         </div>
